7ba17f228da8bc075a2c6b2cf203ad65e222a01c
Alex Constantinou <a_constantinou@hotmail.co.uk>
2020-01-04 15:05:06 +0000

    connected to neo4j

--- api/go.mod
+++ api/go.mod
@@ -1,5 +1,9 @@
-module github.com/aconstantinou123/restfulApi
+module github.com/aconstantinou123/restfulApi/api
 
 go 1.13
 
-require github.com/gorilla/mux v1.7.3
+require (
+	github.com/gorilla/mux v1.7.3 // indirect
+	github.com/neo4j-drivers/gobolt v1.7.4 // indirect
+	github.com/neo4j/neo4j-go-driver v1.7.4 // indirect
+)
--- api/main.go
+++ api/main.go
@@ -6,8 +6,24 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
+	"github.com/neo4j/neo4j-go-driver/neo4j"
+)
+
+var (
+	uri      string = "bolt://neo4j:7687"
+	username string = "neo4j"
+	password string = "test"
+	query    string
+)
+
+var (
+	driver  neo4j.Driver
+	session neo4j.Session
+	result  neo4j.Result
+	err     error
 )
 
 type event struct {
@@ -24,6 +40,35 @@ var events = allEvents{
 		Title:       "Introduction to Golang",
 		Description: "Come join us for a chance to learn how golang works and get to eventually try it out",
 	},
+}
+
+func run() error {
+	if driver, err = neo4j.NewDriver(uri, neo4j.BasicAuth(username, password, ""), func(config *neo4j.Config) {
+		config.Log = neo4j.ConsoleLogger(neo4j.ERROR)
+	}); err != nil {
+		return err
+	}
+	defer driver.Close()
+	if session, err = driver.Session(neo4j.AccessModeWrite); err != nil {
+		return err
+	}
+	defer session.Close()
+
+	result, err = session.Run("CREATE (n:Item { id: $id, name: $name }) RETURN n.id, n.name", map[string]interface{}{
+		"id":   1,
+		"name": "Item 1",
+	})
+	if err != nil {
+		return err // handle error
+	}
+
+	for result.Next() {
+		fmt.Printf("Created Item with Id = '%d' and Name = '%s'\n", result.Record().GetByIndex(0).(int64), result.Record().GetByIndex(1).(string))
+	}
+	if err = result.Err(); err != nil {
+		return err // handle error
+	}
+	return nil
 }
 
 func createEvent(w http.ResponseWriter, r *http.Request) {
@@ -94,6 +139,10 @@ func commonMiddleware(next http.Handler) http.Handler {
 }
 
 func main() {
+	if err := run(); err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
 	port := ":8080"
 	router := mux.NewRouter().StrictSlash(true)
 	router.Use(commonMiddleware)
